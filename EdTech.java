37f00031046a0fbf7130ee4f9302680a5a0b5f8a
rPranto808 <rifatpranto333@gmail.com>
2024-09-08 01:54:44 +0600

    added welcome message

--- EdTech.java
+++ EdTech.java
@@ -1,5 +1,6 @@
 public class EdTech {
     public static void main(String[] args){
         System.out.println("Object Architects!");
+        System.out.println("Welcome to our project!!!wel");
     }
 }
